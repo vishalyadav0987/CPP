3168633a3e798f4e01983d98f8ecc216f0ee0721
vishalyadav0987 <viahalyadav0987@gmail.com>
2023-09-11 12:06:52 +0530

    adding_two _number

---
+++ adding_two _number.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std ;
+int main()
+{
+    int a,b;
+    cout << "Enter 1st number : ";
+    cin>> a;
+    cout << "Enter 2nd number : ";
+    cin >> b;
+    cout <<"The sum of the Number is "<< a+b;
+    return 0;
+}
