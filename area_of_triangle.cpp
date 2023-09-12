58f90818a6f18fbb584dfc6d0fc5f32ff4458966
vishalyadav0987 <viahalyadav0987@gmail.com>
2023-09-12 17:21:40 +0530

    area_of_triangle

---
+++ area_of_triangle.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int h, b;
+    cout << "Enter height of a triangle : ";
+    cin >> h;
+    // cin>>h>>b;
+    cout << "Enter base of a triangle : ";
+    cin >> b;
+    // cout << "Area of Triangle is : ";
+    // cout << 0.5 * (b * h);   // giving answer in (floting value)
+    cout << "Area of Triangle is : " << (b * h) / 2; // giving answer in (interger value)
+    // cout<< "\ n";
+    // cout << "v\n\n7";
+    // cout << "v\t\t7";
+    // cout << "v\r\r7";
+    // cout << "v\b\b7";
+    // cout << "v\a\a7";
+    // cout << "v\'\7";
+    cout << "\n\"K22BW\"";
+}
+
+
+
+
+
+
+  
