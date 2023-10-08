ebc47d7ad5331a859384455837469d761f16e989
vishal <118720096+vishalyadav0987@users.noreply.github.com>
2023-10-08 22:07:11 +0530

    hexOctDec.

---
+++ hexOctDec.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a = 100;
+    int b = 016;
+    cout << a << endl
+         << b << endl;
+         string s;
+         cin>>s;
+         cout<<setw(10)<<s<<endl;
+    cout << "Hexadecimal : " << hex << a << endl;
+    cout << "Octadecimal : " << oct << a << endl;
+    cout << "Decimal : " << dec << a << endl;
+    cout<<setw(10)<<"@"<<endl;
+    cout<<setw(15)<<"@"<<endl;
+}
