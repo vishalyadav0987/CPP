919a2cdf3f26226a65feb2ba58f729f752f436d8
vishal <118720096+vishalyadav0987@users.noreply.github.com>
2023-10-22 23:08:22 +0530

    itrationOfCharacterArray

---
+++ itrationOfCharacterArray.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char c[20]  = "vishal yadav";
+    int i = 0;
+    while(c[i]!=0){
+        cout<<c[i]<<" ";
+        i++;
+    }
+    cout<<"\n";
+    cout<<"The length of array is: "<<i;
+}
