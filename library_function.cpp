143501ba69ea97c3af8e13634738ebd58fdc8f3c
vishal <118720096+vishalyadav0987@users.noreply.github.com>
2023-10-23 23:11:55 +0530

    library_function

---
+++ library_function.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    cout<<pow(2,3)<<endl;
+    cout<<sqrt(16)<<endl;
+    cout<<sqrt(10)<<endl;
+
+    int sq = sqrt(10);
+    cout<<sq<<endl;
+
+    int duobleans =sqrt(10);
+    cout<<duobleans<<endl;
+}
