8a4f72e9a3b25fc5161bc269ab48d54ef6c61afc
vishal <118720096+vishalyadav0987@users.noreply.github.com>
2024-03-26 23:13:11 +0530

    Alphabet Pattern

---
+++ pattern1.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int ch = 65;
+  
+    int n, i, j;
+    cin >> n;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+
+            cout << char(ch+j) << " ";
+        }
+
+        cout << endl;
+        
+    }
+}
+
